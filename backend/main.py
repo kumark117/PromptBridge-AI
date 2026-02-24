8a01fd53bebccd06c2f107ca035003253396ceca
Kumar Krishnamoorthy <kumar.softindies@gmail.com>
2026-02-25 03:28:50 +0530

    added! rm-ed uvicorn.run so Railway can..

--- backend/main.py
+++ backend/main.py
@@ -109,6 +109,6 @@ def generate(request: PromptRequest):
 #         reload=False
 #     )
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run("main:app", host="0.0.0.0", port=port)
